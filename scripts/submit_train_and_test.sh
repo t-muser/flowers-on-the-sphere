798cd8be94281fde9cc1e546829b31847d23e524
t-muser <musertill@gmail.com>
2026-04-29 09:25:40 +0200

    Fix train scripts

--- scripts/submit_train_and_test.sh
+++ scripts/submit_train_and_test.sh
@@ -24,21 +24,17 @@ NAME="planetswe"
 for model in "${MODELS[@]}"; do
     for lr in "${LRS[@]}"; do
         bs=${BATCH_SIZE[$model]}
-        export MODEL_CONFIG="configs/models/${model}.yaml"
-        export DATA_CONFIG="configs/data/planetswe.yaml"
-        export TRAIN_CONFIG="configs/train_4-to-1.yaml"
-        export LR="$lr" BATCH_SIZE="$bs" NAME="$NAME"
 
         TRAIN_JOB=$(sbatch --parsable --time=24:00:00 \
             --job-name="fots-${model}-${lr}" \
-            --export=ALL \
+            --export="MODEL_CONFIG=configs/models/${model}.yaml,DATA_CONFIG=configs/data/planetswe.yaml,TRAIN_CONFIG=configs/train_4-to-1.yaml,LR=${lr},BATCH_SIZE=${bs},NAME=${NAME}" \
             scripts/train.sbatch)
         echo "submitted train ${model} lr=${lr} bs=${bs} -> ${TRAIN_JOB}"
 
         TEST_JOB=$(sbatch --parsable --time=02:00:00 \
             --job-name="fots-test-${model}-${lr}" \
             --dependency=afterok:${TRAIN_JOB} \
-            --export=ALL,TEST_MODE=true \
+            --export="MODEL_CONFIG=configs/models/${model}.yaml,DATA_CONFIG=configs/data/planetswe.yaml,TRAIN_CONFIG=configs/train_4-to-1.yaml,LR=${lr},BATCH_SIZE=${bs},NAME=${NAME},TEST_MODE=true" \
             scripts/train.sbatch)
         echo "submitted test ${model} lr=${lr} -> ${TEST_JOB} (afterok ${TRAIN_JOB})"
     done
